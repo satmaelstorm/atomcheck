4317f6c913aa48de71dddfbcb4b302c1256a9cf6
Vladislav A. Sidorin <vas@rdw.ru>
2019-06-18 14:07:11 +0300

    godocs

--- checker.go
+++ checker.go
@@ -2,11 +2,13 @@ package atomcheck
 
 import "sync"
 
+//Designed to verify that something is running.
 type Check struct {
 	val bool
 	mu sync.Mutex
 }
 
+//Checks its value, if true - returns false, if false - sets its value to true and returns true
 func (c *Check) CheckAndStart() bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -17,6 +19,7 @@ func (c *Check) CheckAndStart() bool {
 	return true
 }
 
+//Sets its value to false
 func (c *Check) End() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
